364fcc288053aa26d75417d381a4bbb45ef3dd91
Aanandhi1201 <aanandhianu480@gmail.com>
2025-09-03 21:20:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function button1(){
+    document.getElementById("bulb").innerHTML='<img src="on-bulb1.webp">';
+}
+function button2(){
+    document.getElementById("bulb").innerHTML='<img src="off-bulb2.webp">';
+}
